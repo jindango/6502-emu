c28e588c565c856b18315b1cc07d2e1dae3885fa
james <jamesdn12@gmail.com>
2020-10-13 23:10:48 -0400

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
 use std::collections::HashMap;
 
-
-
 struct Opcode {
-
-
+    instruction : fn(),
+    addressing_mode : fn(),
+    size : u8,
+    cycles : u8
 }
 
-
-fn gen_opcodes() -> HashMap<u16, u16> {
-    let mut opcodes = HashMap::new();
+fn gen_opcodes() -> HashMap<u16, Opcode> {
+    let mut opcodes = HashMap::new(); 
+    opcodes.insert(0x00, Opcode{instruction : CPU::BRK,
+                                addressing_mode : CPU::Implied,
+                                cycles : 1,
+                                size : 7});
+    opcodes.insert(0x01, Opcode{instruction : CPU::ORA,
+                                addressing_mode : CPU::INDX,
+                                cycles : 2,
+                                size : 6});
+    opcodes.insert(0x05, Opcode{instruction : CPU::ORA,
+                                addressing_mode : CPU::INDX,
+                                cycles : 2,
+                                size : 6});
     return opcodes;
 
-}
-
-enum AddressingMode {
-    Accumulator,     // Accum
-    Imediate,        // IMM
-    Absolute,        // Absolute
-    ZeroPage,        // ZP
-    IndexedZeroPage, // ZP, x or y
-    IndexedAbsolute, // ABS, x or y
-    Implied,         // Implied
-    Relative,        // Relative
-    IndexedIndirect, // IND x
-    IndirectIndexed, // IND y
-    AbsoluteIndirect // Indirect
 }
 
 enum CPUFlag { 
@@ -35,8 +32,8 @@ enum CPUFlag {
     D = 1 << 3, // Decimal mode
     B = 1 << 4, // Break
     U = 1 << 5, // Unused
-        V = 1 << 6, // Overflow
-    N = 1 << 7 // Negative
+    V = 1 << 6, // Overflow
+    N = 1 << 7  // Negative
 }
 
 struct CPU {
@@ -46,15 +43,34 @@ struct CPU {
     y : u8,
     sp : u8,
     pc : u16,
-    status : u8
+    status : u8, 
+
+    // Made u
 }
 
 struct Bus {
     cpu : CPU,
+    // ppu : PPU, // TODO
+    // vmemory : [u8; 64 * 1024], // TODO
     memory : [u8; 64 * 1024], // 64Kb
 }
 
 impl CPU {
+    // Addressing modes
+    fn Accum() {}
+    fn IMM() {}
+    fn ZP() {}
+    fn ZPX() {}
+    fn ZPY() {}
+    fn ABSX() {}
+    fn ABSY() {}
+    fn Implied() {}
+    fn Relative() {}
+    fn INDX() {}
+    fn INDY() {}
+    fn Indirect() {}
+
+    // Instructions
     fn ADC() {}
     fn AND() {}
     fn ASL() {}
@@ -111,12 +127,17 @@ impl CPU {
     fn clock (&self) { 
 
     }
+
+    fn reset(&self) {
+
+    }
 }
 
 impl Bus {
-    fn write(&self, data : u8, address : u8)  {
-        
+    fn write(&mut self, data : u8, address : u8)  {
+        self.memory[address as usize] = data;
     }
+
     fn read(&self, address : u16) -> u8 {
         return self.memory[address as usize];
     }
